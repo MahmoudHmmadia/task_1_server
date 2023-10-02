1399e66606521d65cc1e11df3deb913ee2838adb
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-10-02 09:15:23 +0300

    first

---
+++ server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { Server } from "socket.io";
+import cors from "cors";
+const app = express();
+app.use(cors());
+
+const server = app.listen(3500, () => {
+  console.log("Hey");
+});
+const io = new Server(server, {
+  cors: {
+    origin: "https://task-1-i99r.onrender.com/",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", {
+      message: data,
+      id: new Date().toISOString(),
+    });
+  });
+});
